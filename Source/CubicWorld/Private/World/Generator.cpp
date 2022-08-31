2a6d39f962ffa8469a295a88e92466bfb7eccbb1
Lukas Leitner <lukasleitner17@gmail.com>
2022-08-31 19:37:37 +0200

    change generate only blocks within chunk bounds

--- Source/CubicWorld/Private/World/Generator.cpp
+++ Source/CubicWorld/Private/World/Generator.cpp
@@ -7,12 +7,12 @@
 void UGenerator::GenerateChunk(const FChunkConfig& ChunkConfig, TMap<FIntVector, FBlock>& Blocks)
 {
 	bool hasGeometry = Blocks.Num() > 0 ? true : false;
-	for (int X = -1; X < ChunkConfig.WorldConfig.ChunkSize.X+1; ++X)
+	for (int X = 0; X < ChunkConfig.WorldConfig.ChunkSize.X; ++X)
 	{
-		for (int Y = -1; Y < ChunkConfig.WorldConfig.ChunkSize.Y+1; ++Y)
+		for (int Y = 0; Y < ChunkConfig.WorldConfig.ChunkSize.Y; ++Y)
 		{
 			const FIntVector ChunkPosition = ChunkConfig.GetChunkPositionInBlocks();
-			for (int Z = -1; Z < ChunkConfig.WorldConfig.ChunkSize.Z+1; ++Z)
+			for (int Z = 0; Z < ChunkConfig.WorldConfig.ChunkSize.Z; ++Z)
 			{
 				const FIntVector WorldPosition = FIntVector(X + ChunkPosition.X, Y + ChunkPosition.Y, Z + ChunkPosition.Z);
 				if(const auto block = Blocks.Find(FIntVector(X,Y,Z)); block != nullptr)
